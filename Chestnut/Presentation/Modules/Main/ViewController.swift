f4caed21499d27813b0a5ae13a29d486c8daa7d8
Roman Korostenskyi <romankorostensky@gmail.com>
2020-09-16 18:07:24 +0300

    init commit

---
+++ Chestnut/Presentation/Modules/Main/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Chestnut
+//
+//  Created by Roman Korostenskyi on 16.09.2020.
+//  Copyright © 2020 Roman Korostenskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '12.0'
+use_modular_headers!
+inhibit_all_warnings!
+
+def pods
+
+end
+
+target 'Chestnut' do
+  pods
+end
